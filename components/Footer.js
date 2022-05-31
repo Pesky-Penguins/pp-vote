e854e2d5dac36e6f828f9d85faf886e209484e3f
NFP <nfp@pesky-penguins.com>
2022-05-31 14:07:05 -0400

    Add links to footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,26 @@
+import Link from 'next/link';
+
+const LINKS = {
+  Home: 'https://pesky-penguins.com',
+  Blog: 'https://blog.pesky-penguins.com',
+  Nootopia: 'https://nootopia.pesky-penguins.com',
+  Twitter: 'https://twitter.com/peskypenguins',
+};
+
 export default function Footer() {
   return (
-    <div className="flex flex-col w-full px-8">
+    <div className="flex flex-col w-full px-8 mb-4">
       <div className="divider" />
-      footer
+      <div className="flex justify-around">
+        {Object.entries(LINKS).map(([name, link]) => (
+          <div key={link} className="cursor-pointer hover:text-blue-700">
+            <Link href={link} target="_blank" rel="noreferrer">
+              <p className="font-lucky text-xl underline">{name}</p>
+            </Link>
+          </div>
+        ))}
+      </div>
+      <div className="hidden  bg-error btn-error bg-success btn-success" />
     </div>
   );
 }
