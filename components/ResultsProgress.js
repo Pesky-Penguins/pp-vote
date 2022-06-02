facc05721d1a390501cc16d1f596048676d73fb8
NFP <nfp@pesky-penguins.com>
2022-06-01 21:24:13 -0400

    Fix percentages of votes

--- components/ResultsProgress.js
+++ components/ResultsProgress.js
@@ -32,10 +32,10 @@ export default function ResultsProgress({ className, proposalId, votes, results,
             <div key={o.text} className="flex flex-col items-center justify-center text-center">
               <p className="">{o.text}</p>
               <p className="hidden md:flex font-mono w-full text-center">
-                {counts[i]} ({((counts[i] / votes.length) * 100).toFixed(1)}%)
+                {counts[i]} ({((counts[i] / totalVotes) * 100).toFixed(1)}%)
               </p>
               <p className="flex md:hidden font-mono w-full text-center">
-                {((counts[i] / votes.length) * 100).toFixed(1)}%
+                {((counts[i] / totalVotes) * 100).toFixed(1)}%
               </p>
             </div>
           ))}
